d0ee5dbc0ed1503f788821f757bfaf9818a22872
GalteXx <65533265+GalteXx@users.noreply.github.com>
2020-11-29 16:51:06 +0300

    Update isNumber.cpp

--- check/isNumber.cpp
+++ check/isNumber.cpp
@@ -3,7 +3,7 @@ bool isNumber(string str)
 {
     for(int i = 0; str[i] != '\0'; i++)
     {
-        if((str[i] > 57 || str[i] < 48) && str[i]!=32)
+        if((str[i] > 57 || str[i] < 48) && str[i]!=32 && str[i] != 45)
             return false;
     }
 
