c301791bc5abaf83e8927cac998058361c8f6ca0
GalteXx <65533265+GalteXx@users.noreply.github.com>
2020-11-29 16:50:07 +0300

    Update ft_getline_v_vector.cpp

--- check/ft_getline_v_vector.cpp
+++ check/ft_getline_v_vector.cpp
@@ -1,19 +1,39 @@
 #include"y12.h"
 
-void ft_geline_v_vector(string str,vector<int>&A)
+void ft_geline_v_vector(vector<int>&A)
 {
+    string str;
     int o = 0;
+    bool isNegative = false;
+    getline(cin, str);
     for(int i = 0; str[i] != '\0'; i++)
     {
         if(str[i] == 32)
         {
             A.push_back(o/10);
             o = 0;
+            isNegative = false;
         }
         else if(str[i] != 32)
         {
-            o = o + (int) str[i] - 48;
-            o *= 10;
+            if(str[i] != 45)
+            {
+                if(isNegative)
+                    {
+                        o = o - ((int) str[i] - 48);
+                        o *= 10;
+                    }
+                else
+                    {
+                    o = o + (int) str[i] - 48;
+                    o *= 10;
+                    }
+            }
+            if(str[i - 1] == 45 && (str[i - 2]==32 || i == 1))
+            {
+                o *= -1;
+                isNegative = true;
+            }
         }
     }
     A.push_back(o/10);
