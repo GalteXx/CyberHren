f0c54f2a51d9ac8251e91eae6455617d0caa4f55
Alex <74704132+NoOneSB@users.noreply.github.com>
2020-11-29 20:15:49 +0300

    Add files via upload

---
+++ ПРОЕКТ НОВЫЙ ЭТОТ ПРАВИЛЬНЫЙ/SORTIC/sb.cpp
@@ -0,0 +1,11 @@
+#include"y12.h"
+
+void sb(vector <int> &B)
+{
+    if(B.size()>=2){
+        int tmp;
+        tmp = B[B.size()-2];
+        B[B.size()-2] = B[B.size()-1];
+        B[B.size()-1] = tmp;
+    }
+}
